b8194e7e678295fa7dad57a881f30657e119bc14
bighomeman <bighomeman@163.com>
2018-03-08 16:42:44 +0800

    update parser and merge function

--- project/merge_blacklist.py
+++ project/merge_blacklist.py
@@ -1,14 +1,48 @@
-import subprocess
+import parser_config, os,sys
+from blacklist_tools import *
+sys.dont_write_bytecode = True
+
+# 存储原始数据
+def get_blacklist_module():
+    parse_blacklist = parser_config.blacklist_function
+    for file_name in parse_blacklist:
+         command = 'python .\get_blacklist\\' + file_name +'.py'
+         try:
+              status = os.popen(command)
+              print status.read()
+         except Exception, e:
+                print e
+
+def merge_blacklist(dir,date,name):
+    parse_blacklist = parser_config.blacklist_function
+    i = 0
+    merge_result = {}
+    for file_name in parse_blacklist:
+        result = load_dict(file_name + '.json')
+        if i ==0:
+            merge_result = result
+        else:
+            merge_result = update_dict(result,merge_result)
+    saveAsJSON(date,merge_result,dir,name)
+
+    for file_name in parse_blacklist:
+        if os.path.exists(file_name+'.json'):
+            os.remove(file_name+'.json')
+
+#建Trie树
+
+def store_trie(dir, date, name):
+    source_store_path =parser_config.source_store_path
+    result = load_dict(source_store_path[1] + source_store_path[0] + "-" +date + '.json')
+    saveAsJSON(date,create_Trie(result),dir,name)
 
 
-shell_cmd = 'python .\get_blacklist\\malwaredomains.py'
-p = subprocess.Popen(shell_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-while p.poll() is None:
-    line = p.stdout.readline()
-    line = line.strip()
-    if line:
-        print('Subprogram output: [{}]'.format(line))
-if p.returncode == 0:
-    print('Subprogram success')
-else:
-    print('Subprogram failed')
+
+if __name__ == '__main__':
+    source_store_path =parser_config.source_store_path
+    trie_store_path = parser_config.trie_store_path
+    get_blacklist_module()
+    merge_blacklist(source_store_path[1],sys.argv[1],source_store_path[0])
+    store_trie(trie_store_path[1],sys.argv[1],trie_store_path[0])
+
+
--- project/parser_config.py
+++ project/parser_config.py
@@ -5,7 +5,20 @@ cp = ConfigParser.SafeConfigParser()
 cp.read("blacklist_match.conf")
 sections =  cp.sections()
 # print sections[0]
-parameter = cp.options(sections[0])
+parse_blacklist = cp.options(sections[0])
 # print type(parameter[0])
 # print 'items of [ssh]:', cp.items('parse_blacklist')    # items of [ssh]: [('host', '192.168.1.101'), ('user', 'huey'), ('pass', 'huey')]
-# print cp.get("parse_blacklist", parameter[0])
+blacklist_function = []
+for temp in parse_blacklist:
+    blacklist_function.append(cp.get("parse_blacklist", temp))
+
+source_store_path_key = cp.options(sections[1])
+source_store_path = []
+for temp in source_store_path_key:
+    source_store_path.append(cp.get('source_store_path', temp))
+
+
+trie_store_path_key = cp.options(sections[2])
+trie_store_path  = []
+for temp in trie_store_path_key:
+    trie_store_path.append('Trie_store_path', temp)
